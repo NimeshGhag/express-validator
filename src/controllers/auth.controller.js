8542f4e3f80d5131635be0edcfa42564c6ffd1ac
NimeshGhag <ghagnimesh490@gmail.com>
2026-03-31 12:08:41 +0530

    feat: implement user registration controller and route

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+const registerController = (req, res) => {
+  const { username, email, password } = req.body;
+
+  return res.status(200).json({
+    message: "User registered successfully",
+  });
+};
+
+module.exports = {
+  registerController,
+};
--- src/routes/dummy.route.js
+++ src/routes/dummy.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
+const { registerController } = require("../controllers/auth.controller");
+
 const router = express.Router();
 
-router.post("/register");
+router.post("/register", registerController);
 
 module.exports = router;
